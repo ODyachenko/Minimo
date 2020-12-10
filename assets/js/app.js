596d1c418566d405ecf0e9f59dd5e1cd65d1091f
ODyachenko <oleg1696@gmail.com>
2020-12-10 20:19:10 +0200

    Assets for Minimo

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#modal").show();
+    $(".close").click(function(){
+        $("#modal").hide();
+    });
+    $("#close").click(function(){
+        $("#modal").hide();
+    });
+});
+
